e86e15cf018ad6ed7a1681b22d14767f46e7e6c1
Keith Alfaro <keith4895@gmail.com>
2019-01-19 13:51:58 -0500

    remove OnNodeModified

--- NodeModelCharts/Nodes/PieChartNodeModel.cs
+++ NodeModelCharts/Nodes/PieChartNodeModel.cs
@@ -36,7 +36,6 @@ namespace NodeModelCharts.Nodes
             {
                 inputValue = value;
                 RaisePropertyChanged("InputValue");
-                OnNodeModified();
             }
         }
         #endregion
